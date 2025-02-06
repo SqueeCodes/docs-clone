31473aeed9560196fe5d7fcc7e0035ecebb16b79
squeecodes <itsmesquee@gmail.com>
2025-02-06 10:14:12 -0700

    changed default exporting for LineHeightIcon

--- src/components/icons/line-height-icon.tsx
+++ src/components/icons/line-height-icon.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 
-export const LineHeightIcon = () => {
+const LineHeightIcon = () => {
   return (
-   
-<svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 10V5m0 0L4 7m2-2 2 2m-2 7v5m0 0 2-2m-2 2-2-2m8-10h8m0 5h-8m0 5h8"/></svg>
+    <svg
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+    >
+      <path
+        stroke="#000000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M6 10V5m0 0L4 7m2-2 2 2m-2 7v5m0 0 2-2m-2 2-2-2m8-10h8m0 5h-8m0 5h8"
+      />
+    </svg>
   );
- };
+};
+
+export default LineHeightIcon;
