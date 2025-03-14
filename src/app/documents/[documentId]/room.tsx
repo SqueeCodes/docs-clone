f1b6b0ee912014cc614042d428022eff9572d26f
squeecodes <itsmesquee@gmail.com>
2025-03-14 15:26:50 -0600

    fixed authorizing bug

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -11,7 +11,9 @@ import { useParams } from "next/navigation";
 export function Room({ children }: { children: ReactNode }) {
  const params = useParams();
   return (
-    <LiveblocksProvider publicApiKey={"pk_dev_XUXm3bsSEWx3c0lCrjbiQH7ZDacBPrLd6hneByW54qo3Yvdx9DSGUCvwVYkShmhj"}>
+    <LiveblocksProvider 
+    throttle={16}
+    authEndpoint="/api/liveblocks-auth"    >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
