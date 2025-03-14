097539ea3a4e2fd49d89608c465c809d210249dd
squeecodes <itsmesquee@gmail.com>
2025-03-14 17:05:17 -0600

    added avatar icons and comment tagging functionalities

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -39,6 +39,7 @@ import { useEditorStore } from "@/store/use-editor-store";
 
 import { DocumentInput } from "./document-input";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Avatars } from "./avatars";
 
 export const Navbar = () => {
   const { editor } = useEditorStore();
@@ -240,6 +241,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
