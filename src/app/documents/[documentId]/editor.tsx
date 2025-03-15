dead62dcae4ce15398692a9aabbbf0d9fee5d857
squeecodes <itsmesquee@gmail.com>
2025-03-15 12:07:00 -0600

    finished liveblock integration

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -28,12 +28,22 @@ import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from '@/constants/margins';
 import { Ruler } from './ruler';
 import { Threads } from './threads';
 
-export const Editor = () => {
-  const liveblocks = useLiveblocksExtension();
+interface EditorProps {
+  initialContent?: string | undefined;
+};
 
+export const Editor = ({ initialContent }: EditorProps) => {
+  const leftMargin = useStorage((root) => root.leftMargin) ?? LEFT_MARGIN_DEFAULT;
+  const rightMargin = useStorage((root) => root.rightMargin) ?? RIGHT_MARGIN_DEFAULT;
+  
+  const liveblocks = useLiveblocksExtension({
+    initialContent,
+    offlineSupport_experimental: true,
+  });
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    autofocus: true,
     immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
@@ -42,28 +52,27 @@ export const Editor = () => {
       setEditor(null);
     },
     onUpdate({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     onSelectionUpdate({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     onTransaction({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     onFocus({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     onBlur({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     onContentError({ editor }) {
-      setEditor(editor);
+      setEditor(editor)
     },
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right: 56px;",
-        class:
-          "focus:outline-none print:border-0 bg-white border rounded-md shadow-lg border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pb-10 cursor-text",
+        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
+        class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
       },
     },
     extensions: [
@@ -71,17 +80,15 @@ export const Editor = () => {
       StarterKit.configure({
         history: false,
       }),
-      LineHeightExtension.configure({
-        types: ["heading", "paragraph"],
-      }),
+      LineHeightExtension,
       FontSizeExtension,
       TextAlign.configure({
-        types: ["heading", "paragraph"],
+        types: ["heading", "paragraph"]
       }),
       Link.configure({
         openOnClick: false,
         autolink: true,
-        defaultProtocol: "https",
+        defaultProtocol: "https"
       }),
       Color,
       Highlight.configure({
@@ -104,11 +111,11 @@ export const Editor = () => {
   })
 
   return (
-    <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible"'>
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
       <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
-        <Threads editor={editor}/>
+        <Threads editor={editor} />
       </div>
     </div>
   );
